bc8f9897ab6592e8a7cd275b9f27f37f82c90e9f
Hongliang Wang <hongliangwang@yunify.com>
2018-10-22 12:33:40 +0800

    Use Generated Random Token for Minio

--- 1.1.1/vm-image/opt/harbor/bin/app.sh
+++ 1.1.1/vm-image/opt/harbor/bin/app.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
+source /opt/harbor/bin/env.sh
 minioVersion="RELEASE.2018-10-06T00-15-16Z"
 
-storageInit() {
-  mkdir -p /data/storage/harbor
+[[ "$NODE_ROLE" == "$1_node" ]] || exit 0
+
+genTokenWithLength() {
+  < /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-$1}
+}
+
+storageGenerateToken() {
+  accessKey=$(genTokenWithLength 21)
+  secretKey=$(genTokenWithLength 41)
+
+  cat << EOF
+{"accessKey":"$accessKey","secretKey":"$secretKey"}
+EOF
 }
 
 storageStart() {
+  mkdir -p /data/storage/harbor
+
   docker run -d \
     --name minio \
-    -e "MINIO_ACCESS_KEY=admin" \
-    -e "MINIO_SECRET_KEY=password" \
+    -e "MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY" \
+    -e "MINIO_SECRET_KEY=$MINIO_SECRET_KEY" \
     -v /data/storage:/data \
     -p 80:9000 \
     --restart always \
